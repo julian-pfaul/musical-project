c1b20304713f316b540a22fb82d53088832d2a83
PfaulJulian <pfaul.julian@gmail.com>
2025-08-11 15:55:17 +0200

    incorporated libtorch

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-cmake -DCMAKE_PREFIX_PATH=/
+#!/usr/bin/sh
+
+set -e
+
+libtorch_path="$(cat LIBTORCH_PATH)/share/cmake/Torch"
+
+echo ${libtorch_path}
+
+mkdir -p build
+cd build
+
+cmake -DCMAKE_PREFIX_PATH="${libtorch_path}" ..
+cmake --build . --config Release
--- source/time_unit.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class time_unit {
-public:
-private:
-};
